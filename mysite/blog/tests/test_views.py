d3edbbf39db20a24bc6b17e9ca705ee485cbd13f
Karthik <karthikeyan@testpress.in>
2022-11-14 18:52:54 +0530

    Refactor the testcases

--- mysite/blog/tests/test_views.py
+++ mysite/blog/tests/test_views.py
@@ -1,18 +1,16 @@
 from django.test import TestCase
 from django.urls import reverse
 from blog.models import Post
-from blog.tests.test_Model_Mixin_TestCases import ModelMixinTestCase
+from blog.tests.test_modelmixintestcase  import ModelMixinTestCase
 
 
 class TestViews(ModelMixinTestCase, TestCase):
-    def test_List_View_Get(self):
+    def test_templates_used_for_list_view(self):
 
-        response = self.client.get(self.listurl)
-
-        self.assertEquals(response.status_code, 200)
+        response = self.client.get(self.post_list_url)
         self.assertTemplateUsed(response, "blog/post/list.html")
 
-    def test_detail_View_Get(self):
+    def test_templates_used_for_detail_view(self):
 
         response = self.client.get(self.post_detail_url)
 
