22fb0b2f668518f518c1f69a86ac54c0728dee64
Karthik <karthikeyan@testpress.in>
2022-11-15 17:07:25 +0530

    Refactor Testcases for urls and views

--- mysite/blog/tests/test_urls.py
+++ mysite/blog/tests/test_urls.py
@@ -1,12 +1,22 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from blog.views import post_list, post_detail
-from blog.tests.test_modelmixintestcase  import ModelMixinTestCase
+from blog.tests.test_modelmixintestcase import ModelMixinTestCase
 
 
 class Testurls(ModelMixinTestCase, SimpleTestCase):
     def test_list(self):
-        self.assertEqual((resolve(self.post_list_url ).func), post_list)
+        self.post_list_url = reverse("blog:post_list")
+        self.assertEqual((resolve(self.post_list_url).func), post_list)
 
     def test_detail(self):
+        self.post_detail_url = reverse(
+            "blog:post_detail",
+            args=[
+                self.published_post.publish.year,
+                self.published_post.publish.month,
+                self.published_post.publish.day,
+                self.published_post.slug,
+            ],
+        )
         self.assertEqual((resolve(self.post_detail_url).func), post_detail)
