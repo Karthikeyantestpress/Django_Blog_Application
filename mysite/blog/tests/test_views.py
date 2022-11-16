87953d715ecfd91fbc279d32b6fab7c29e7ce509
Karthikeyantestpress <112085200+Karthikeyantestpress@users.noreply.github.com>
2022-11-16 19:20:10 +0530

    Create list and detail view (#5)
    * Create list and detail view
    
    * Map URLs to views
    
    * Create templates and static files
    
    * Refactor the testcases
    
    * Format html files
    
    * Refactor Testcases for urls and views
    
    * refactor :fix Testcase issue in modelmixin

---
+++ mysite/blog/tests/test_views.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.urls import reverse
+from blog.models import Post
+from blog.tests.test_modelmixintestcase import ModelMixinTestCase
+
+
+class TestViews(ModelMixinTestCase, TestCase):
+    def test_templates_used_for_list_view(self):
+        self.post_list_url = reverse("blog:post_list")
+
+        response = self.client.get(self.post_list_url)
+        self.assertTemplateUsed(response, "blog/post/list.html")
+
+    def test_templates_used_for_detail_view(self):
+        self.post_detail_url = reverse(
+            "blog:post_detail",
+            args=[
+                self.published_post.publish.year,
+                self.published_post.publish.month,
+                self.published_post.publish.day,
+                self.published_post.slug,
+            ],
+        )
+
+        response = self.client.get(self.post_detail_url)
+
+        self.assertTemplateUsed(response, "blog/post/detail.html")
